9200b88e3d220a22be6e45a76b54c95031983aa2
finiteduck <vincent.lanore@ens-lyon.org>
2018-09-20 00:18:39 +0200

    Added actual SDL graphical stuff in main + minor improvements to GlobalSDL.

--- GlobalLib.cpp
+++ GlobalLib.cpp
@@ -14,21 +14,24 @@ class GlobalSDL : public GlobalLib {
         }
     }
 
-    [[noreturn]] void fail_sdl(std::string message) {
+    void fail_sdl(std::string message) {
         std::cerr << message << std::endl;
         std::string e = SDL_GetError();
         if (e != "") { std::cerr << e << std::endl; }
         exit(1);
     }
 
-    [[noreturn]] void fail_ttf(std::string message) {
+    void fail_ttf(std::string message) {
         std::cerr << message << std::endl;
         std::string e = TTF_GetError();
         if (e != "") { std::cerr << e << std::endl; }
         exit(1);
     }
 
-    ~GlobalSDL() { SDL_Quit(); }
+    ~GlobalSDL() {
+        TTF_Quit();
+        SDL_Quit();
+    }
 };
 
 std::unique_ptr<GlobalLib> make_global_sdl(GlobalSDLFlags flags) {
--- main.cpp
+++ main.cpp
@@ -8,6 +8,19 @@ using namespace std;
 int main(int, char**) {
     auto g = make_global_sdl(USE_TTF);
 
-    cout << "Hello world\n";
+    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xAA, 0xAA));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+
     return 0;
 }
